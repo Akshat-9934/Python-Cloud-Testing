ee72935b94c5f7069b30144d13998e1fd22a8395
Akshat-9934 <akki18edu@gmail.com>
2026-03-18 12:20:18 +0530

    Update FastAPI routes and add addition endpoint

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/message")
-def get_message():
-    return {"text": "Hello from Python 🚀"}
+@app.get("/")
+def home():
+    return {"message": "Python running in cloud!"}
+
+@app.get("/add")
+def add(a: int, b: int):
+    return {"result": a + b}
